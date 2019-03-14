74bd047e1adf39dacf82987fd3243e8d736e14b9
Jake <jaggre@live.com>
2019-03-14 16:56:15 -0700

    Update conchbot.js

--- conchbot.js
+++ conchbot.js
@@ -4,7 +4,7 @@ const prefix = "%";
 
 client.on("ready", () => {
   console.log("I am ready!");
-    client.user.setGame('Looking for something to eat');
+    client.user.setActivity('Looking for something to eat');
 });
 
 
