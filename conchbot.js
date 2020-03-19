238f3d6315d79427ccb1c399947ac80221d55329
jake <jaggre@live.com>
2020-03-19 09:56:50 -0700

    Update conchbot.js

--- conchbot.js
+++ conchbot.js
@@ -97,5 +97,27 @@ if( cancelWords.some(word => message.content.includes(prefix + word)) ) {
 
     });
 
+    client.on("message", (message) => {
+
+      const uwuWords = ["uwu" ];
+      
+          
+      const uwuResponses = ["The UWU POLICE are on the scene! PLEASE KEEP YOUR UWUs TO YOURSELVES AT ALL TIMES." ];
+      
+      const uwuimageResponses = [{files: ["https://i.imgur.com/dJrpw2t.jpg"]}];
+      
+          /* TO ADD: Random Image Selection
+          "https://i.imgur.com/RRyVUxw.png"  */  
+          
+      if( uwuWords.some(word => message.content.includes(prefix + word)) ) {
+        var response = uwuResponses [Math.floor(Math.random()*uwuResponses .length)];
+       
+          var uwus = uwuimageResponses
+       [Math.floor(Math.random()*uwuimageResponses .length)];
+                  message.channel.send(response,uwus).then().catch(console.error);
+        // Or just do message.delete();
+      }
+          });
+
 
 client.login(process.env.BOT_TOKEN);
